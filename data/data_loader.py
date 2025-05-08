81713d8c373e223e5a48ec70ababa5d271b148d8
Roshita <roshita1608@gmail.com>
2025-05-08 15:52:20 -0400

    updated dataloader

--- data/data_loader.py
+++ data/data_loader.py
@@ -6,8 +6,8 @@ import json
 class DataLoader:
     def __init__(self):
         self.mimic_cxr_image_data = None
-        self.mimic_cxr_image_dir = './mimic-cxr-images-512/'
-        self.mimic_cxr_report_dir = './mimic-cxr/'
+        self.mimic_cxr_image_dir = './datasets/mimic-cxr-images-512/'
+        self.mimic_cxr_report_dir = './datasets/mimic-cxr/'
         self.mimic_cxr_pickle_path = 'saved_dataset.pkl'
 
     def load_mimic_cxr_from_hf(self):
@@ -33,7 +33,7 @@ class DataLoader:
 
         print("----Saving Complete---")
     
-    def create_report_image_mapping(self, sample_size=100):
+    def create_report_image_mapping(self, sample_size=377110):
         data = []
         patient_groups = sorted(os.listdir(os.path.join(self.mimic_cxr_image_dir,'files'))) # [p10,p11..]
         count = 0
@@ -52,25 +52,26 @@ class DataLoader:
 
                     study_ids = sorted(os.listdir(patient_folder_path))
                     for study_id in study_ids:
-                        study_id_path = os.path.join(patient_folder_path, study_id)
-                        file = sorted(os.listdir(study_id_path))[0]
-                        if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.png'):
-                            image_path = os.path.join(study_id_path, file)
+                        if study_id != '.DS_Store':
+                            study_id_path = os.path.join(patient_folder_path, study_id)
+                            file = sorted(os.listdir(study_id_path))[0]
+                            if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.png'):
+                                image_path = os.path.join(study_id_path, file)
 
-                            # Corresponding report path
-                            report_path = os.path.join(
-                                self.mimic_cxr_report_dir, 'files', patient_group, patient_folder, f"{study_id}.txt"
-                            )
-                            if not os.path.exists(report_path):
-                                continue
+                                # Corresponding report path
+                                report_path = os.path.join(
+                                    self.mimic_cxr_report_dir, 'files', patient_group, patient_folder, f"{study_id}.txt"
+                                )
+                                if not os.path.exists(report_path):
+                                    continue
 
-                            entry = {
-                                "id": count,
-                                "report_path": report_path,
-                                "image_path": image_path
-                            }
-                            data.append(entry)
-                            count += 1
+                                entry = {
+                                    "id": count,
+                                    "report_path": report_path,
+                                    "image_path": image_path
+                                }
+                                data.append(entry)
+                                count += 1
 
                         if count >= sample_size:
                             break
@@ -79,7 +80,7 @@ class DataLoader:
                 if count >= sample_size:
                         break
             
-        with open("image_report_mapping.json", "w") as f:
+        with open("./image_report_mapping_all_377110.json", "w") as f:
             json.dump(data, f, indent=4)
 
         print(f"Saved {len(data)} entries to image_report_mapping.json")
@@ -97,7 +98,7 @@ def main():
         loader.save_mimic_cxr_to_pickle()
     
     # loader.save_mimic_cxr_images_to_jpeg()
-    # loader.create_report_image_mapping()
+    loader.create_report_image_mapping()
 
 if __name__ == "__main__":
     main()
