36d676221e9ea7147d84d9af0f414a5001ff3b98
Roshita <roshita1608@gmail.com>
2025-05-08 23:00:08 -0400

    run script - updated

--- run.py
+++ run.py
@@ -25,15 +25,19 @@ def main():
     else:
         indexer._build_index()
 
-    test_image = "./datasets/mimic-cxr-images-512/files/p19/p19000065/s51613820/58f383e7-edcbd8c7-2f6dc2af-eb97ddf1-f7cbc46a.jpg"
-    test_report = "./datasets/mimic-cxr/files/p19/p19000065/s51613820.txt"
+    test_image = "./datasets/mimic-cxr-images-512/files/p19/p19000065/s51613820/58f383e7-edcbd8c7-2f6dc2af-eb97ddf1-f7cbc46a.jpg" #TODO: add your path
+    test_report = "./datasets/mimic-cxr/files/p19/p19000065/s51613820.txt" #TODO: add your path
     retriever = Retriever(indexer, test_image, test_report, top_k=5, metric="cosine")
     results = retriever.retrieve_similar_items()
     
     for r in results:
+        new_report_path = os.path.join('./all_reports', r['report_path'].split('/')[-1])
+        with open(new_report_path, 'r') as f:
+            report_data = f.read()
         print(f"Rank {r['rank']}:")
         print(f"  Image: {r['image_path']}")
         print(f"  Report: {r['report_path']}")
+        print(f"  Report Content: {report_data}") # TODO: Can save this REPORT CONTENT
         print(f"  Score: {r['score']:.4f}\n")
 
 if __name__ == "__main__":
